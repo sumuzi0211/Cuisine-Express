0b65bd9153af410d8d42906bcb79c5d5f83d5af7
sumuzi0211 <sumuzi0211@163.com>
2024-04-17 14:54:41 +0800

    删除测试代码

--- sky-server/src/main/java/com/sky/SkyApplication.java
+++ sky-server/src/main/java/com/sky/SkyApplication.java
@@ -14,6 +14,5 @@ public class SkyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkyApplication.class, args);
         log.info("server started");
-        //testtest
     }
 }
